d8b19983a3745cbc5d38b9ff2a603f38f2d8cfe8
HubHop <zizhengpan98@gmail.com>
2021-07-25 19:45:23 +1000

    add visualization code

--- memory_saving/models/vision_transformer.py
+++ memory_saving/models/vision_transformer.py
@@ -31,6 +31,7 @@ from timm.models.resnet import resnet26d, resnet50d
 from timm.models.registry import register_model
 
 import memory_saving as ms
+import numpy as np
 
 __all__ = [
     'deit_ms_tiny_patch16_224',
@@ -80,6 +81,180 @@ default_cfgs = {
     'vit_base_resnet26d_224': _cfg(),
     'vit_base_resnet50d_224': _cfg(),
 }
+
+
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns, numpy as np
+
+random_colors = np.random.rand(12, 3).tolist()
+
+save_root = '/data1/quant/plots'
+
+colors = [
+(0.43, 0.83, 0., 0.3),
+(0.97, 0.71, 0., 0.3),
+(0.2, 0.77, 1.0, 0.3),
+]
+
+def plot_act_dist(name, output):
+    if not os.path.exists(save_root):
+        os.mkdir(save_root)
+    plt.clf()
+    ax = sns.histplot(output.mean(dim=0).flatten().detach().cpu().numpy(), element="step", kde=False)
+    img_name = f"{name}.png"
+    plt.title(f"{name}")
+    plt.xlabel('Activations')
+    plt.savefig(os.path.join(save_root, img_name))
+
+def plot_act_dist_per_head(name, output):
+    if not os.path.exists(save_root):
+        os.mkdir(save_root)
+    plt.clf()
+    fig, ax = plt.subplots()
+
+    if len(output.shape) == 4:
+        B, H, N, _ = output.shape
+        data = output
+        for i in range(H):
+            attn = data[:, i, ...].mean(dim=0).flatten().detach().cpu().numpy()
+            sns.histplot(attn, element="step", kde=False, color=(*random_colors[i], 0.3), ax=ax)
+    else:
+        groups = 12
+        B, N, C = output.shape
+        data = output.reshape(B, N, groups, C//groups).permute(0,2,1,3)
+        for i in range(groups):
+            attn = data[:, i, ...].mean(dim=0).flatten().detach().cpu().numpy()
+            sns.histplot(attn, element="step", kde=False, color=(*random_colors[i], 0.3), ax=ax)
+
+    plt.title(f"{name}")
+    plt.xlabel('Activations')
+    img_name = f"{name}.png"
+    plt.savefig(os.path.join(save_root, img_name))
+
+class AnalyseMlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=ms.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = ms.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = ms.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        plot_act_dist_per_head(f'{self.name}-in', x)
+
+        plot_act_dist_per_head(f'{self.name}-before-fc1', x)
+        x = self.fc1(x)
+
+        plot_act_dist_per_head(f'{self.name}-before-gelu', x)
+        x = self.act(x)
+
+        x = self.drop(x)
+
+        plot_act_dist_per_head(f'{self.name}-before-fc2', x)
+        x = self.fc2(x)
+
+        x = self.drop(x)
+
+        plot_act_dist_per_head(f'{self.name}-out', x)
+        return x
+
+class AnalyseAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        # NOTE scale factor was wrong in my original version, can set manually to be compat with prev weights
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.qkv = ms.Linear(dim, dim * 3, bias=qkv_bias)
+        self.softmax = ms.Softmax(dim=-1)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = ms.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+        self.mm1 = ms.MatMul()
+        self.mm2 = ms.MatMul()
+
+    def forward(self, x):
+        B, N, C = x.shape
+        plot_act_dist_per_head(f'{self.name}-before-qkv', x)
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]   # make torchscript happy (cannot use tensor as tuple)
+
+        plot_act_dist_per_head(self.name + '-before-q@k-q', q)
+        plot_act_dist_per_head(self.name + '-before-q@k-k', k)
+        attn = self.mm1(q, k.transpose(-2, -1)) * self.scale
+
+        plot_act_dist_per_head(f'{self.name}-before-softmax', attn)
+        attn = self.softmax(attn)
+
+        attn = self.attn_drop(attn)
+
+        plot_act_dist_per_head(f'{self.name}-before-attn@v-attn', attn)
+        plot_act_dist_per_head(f'{self.name}-before-attn@v-v', v)
+        x = self.mm2(attn, v).transpose(1, 2).reshape(B, N, C)
+
+        plot_act_dist_per_head(f'{self.name}-before-proj', x)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+
+class AnalyseBlock(nn.Module):
+
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=ms.GELU, norm_layer=ms.LayerNorm):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = AnalyseAttention(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = AnalyseMlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+
+    def forward(self, x):
+        plot_act_dist_per_head(f'{self.name}-before-attn', x)
+        residual_1 = self.drop_path(self.attn(self.norm1(x)))
+
+        plot_act_dist_per_head(f'{self.name}-before-res-add-1', residual_1)
+        x = x + residual_1
+
+        plot_act_dist_per_head(f'{self.name}-before-mlp', x)
+        residual_1 = self.drop_path(self.mlp(self.norm2(x)))
+
+        plot_act_dist_per_head(f'{self.name}-before-res-add-2', residual_1)
+        x = x + residual_1
+
+        plot_act_dist_per_head(f'{self.name}-out', x)
+        return x
+
+class AnalysePatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        plot_act_dist_per_head(f'{self.name}-in', x)
+        x = self.proj(x).flatten(2).transpose(1, 2)
+        plot_act_dist_per_head(f'{self.name}-out', x)
+        return x
+
 
 
 class Mlp(nn.Module):
@@ -210,6 +385,85 @@ class HybridEmbed(nn.Module):
         return x
 
 
+class AnalyseViT(nn.Module):
+    """ Vision Transformer with support for patch or hybrid CNN input stage
+    """
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
+                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
+                 drop_path_rate=0., hybrid_backbone=None, norm_layer=ms.LayerNorm):
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+
+        self.patch_embed = AnalysePatchEmbed(
+            img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
+        self.pos_drop = nn.Dropout(p=drop_rate)
+
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList([
+            AnalyseBlock(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+
+        # Classifier head
+        self.head = ms.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        trunc_normal_(self.cls_token, std=.02)
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = ms.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        plot_act_dist_per_head(f'{self.name}-in', x)
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x)
+        x = x[:, 0]
+        plot_act_dist(f'{self.name}-out', x)
+        return x
+
+    def forward(self, x):
+        plot_act_dist_per_head(f'vit-in', x)
+        x = self.forward_features(x)
+        x = self.head(x)
+        plot_act_dist(f'vit-out', x)
+        return x
+
+
 class VisionTransformer(nn.Module):
     """ Vision Transformer with support for patch or hybrid CNN input stage
     """
@@ -300,6 +554,15 @@ def _conv_filter(state_dict, patch_size=16):
             v = v.reshape((v.shape[0], 3, patch_size, patch_size))
         out_dict[k] = v
     return out_dict
+
+
+@register_model
+def deit_ms_tiny_analyse(pretrained=False, **kwargs):
+    model = AnalyseViT(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    return model
 
 
 @register_model
