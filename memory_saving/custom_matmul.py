fix gradient error

memory_saving/custom_matmul.py
@@ -39,7 +39,7 @@ class matmul(torch.autograd.Function):
 
         if ctx.needs_input_grad[4] and grad_input1 is not None:
             grad_clip1 = custom_quant.Quant.backward(ctx, grad_input1, '_1')
-        if ctx.needs_input_grad[8] and grad_input2 is not None:
+        if ctx.needs_input_grad[10] and grad_input2 is not None:
             grad_clip2 = custom_quant.Quant.backward(ctx, grad_input2, '_2')
 
         return grad_input1, grad_input2, None, None, grad_clip1, None, None,None, None, None, grad_clip2, None, None, None, None

memory_saving/custom_quant.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,6 +9,8 @@ if 'memory_saving' not in __name__:
 else:
     from . import packbit
     from . import native
+import pydevd
+
 
 class Quant(object):
     def __init__(self, memory_saving=False, args=None, logger=None, enable=False, tag='fm'):
@@ -30,7 +31,7 @@ class Quant(object):
         self.logger = logger
         self.stable = -1
         self.correlate = 1.0
-        self.warmup_choice = 'MA' # or 'EMA'
+        self.warmup_choice = 'MA'  # or 'EMA'
         self.ema_decay = 0.9999
         self.requires_grad = False
 
@@ -43,7 +44,7 @@ class Quant(object):
                 self.logger = args.logger
             else:
                 if args is None:
-                    self.logger = logger_wrapper() 
+                    self.logger = logger_wrapper()
                 else:
                     logger_root = args.logger_root + '.' if hasattr(args, 'logger_root') else ''
                     self.logger = logging.getLogger(logger_root + __name__)
@@ -66,12 +67,15 @@ class Quant(object):
                 self.non_negative_only = self.non_negative_only and args.nno
             if hasattr(args, 'fm_half_range'):
                 self.non_negative_only = self.non_negative_only and args.fm_half_range
-            #if self.level > 256:
+            # if self.level > 256:
             #    self.level = 257
-            
-            self.verbose("index({})-clip_val({})-level({})-stable({})-correlate({})-non_negative_only({})-requires_grad({})".format(
-                self.index, self.clip_val.item(), self.level, self.stable, self.correlate, self.non_negative_only, self.requires_grad))
-        self.items = ['clip_val', 'level', 'stable', 'correlate', 'non_negative_only', 'warmup_choice', 'ema_decay', 'requires_grad']
+
+            self.verbose(
+                "index({})-clip_val({})-level({})-stable({})-correlate({})-non_negative_only({})-requires_grad({})".format(
+                    self.index, self.clip_val.item(), self.level, self.stable, self.correlate, self.non_negative_only,
+                    self.requires_grad))
+        self.items = ['clip_val', 'level', 'stable', 'correlate', 'non_negative_only', 'warmup_choice', 'ema_decay',
+                      'requires_grad']
         self.clip_val.requires_grad = self.enable and self.requires_grad
 
     def __str__(self):
@@ -126,7 +130,7 @@ class Quant(object):
         index = self.index
         if 'index' in parameters:
             if isinstance(parameters['index'], int):
-                index =  parameters['index']
+                index = parameters['index']
             elif isinstance(parameters['index'], dict) and self.tag in parameters['index']:
                 index = parameters['index'][self.tag]
         if index != self.index:
@@ -145,59 +149,59 @@ class Quant(object):
 
             if by_index == 'all' or self.index in by_index:
                 if ('by_tag' in parameters and self.tag in parameters['by_tag']) or ('by_tag' not in parameters):
-                        for k, v in list(parameters.items()):
-                            if hasattr(self, "{}".format(k)):
-                                if isinstance(getattr(self, k), bool):
-                                    v = False if v in ['False', 'false', False] else True
-                                elif isinstance(getattr(self, k), int):
-                                    v = int(v)
-                                elif isinstance(getattr(self, k), float):
-                                    v = float(v)
-                                elif isinstance(getattr(self, k), str):
-                                    v = v.replace("'", "").replace('"', '')
-                                    if 'same' in v:
-                                        v = v.replace('same', str(self.index))
-                                    elif "last" in v:
-                                        v = v.replace('last', str(self.index-1))
-                                if isinstance(getattr(self, k), torch.Tensor):
-                                    with torch.no_grad():
-                                        if getattr(self, 'progressive', False):
-                                            if 'lsq' in self.args.keyword or '{}_lsq'.format(self.tag) in self.args.keyword:
-                                                if k in ['level_num']:
-                                                    #if hasattr(self, 'clip_val'):
-                                                    v = float(v)
-                                                    # if negative number provide, it indicates decreasing on current
-                                                    v = v if v > 0 else self.level_num.item() + v
+                    for k, v in list(parameters.items()):
+                        if hasattr(self, "{}".format(k)):
+                            if isinstance(getattr(self, k), bool):
+                                v = False if v in ['False', 'false', False] else True
+                            elif isinstance(getattr(self, k), int):
+                                v = int(v)
+                            elif isinstance(getattr(self, k), float):
+                                v = float(v)
+                            elif isinstance(getattr(self, k), str):
+                                v = v.replace("'", "").replace('"', '')
+                                if 'same' in v:
+                                    v = v.replace('same', str(self.index))
+                                elif "last" in v:
+                                    v = v.replace('last', str(self.index - 1))
+                            if isinstance(getattr(self, k), torch.Tensor):
+                                with torch.no_grad():
+                                    if getattr(self, 'progressive', False):
+                                        if 'lsq' in self.args.keyword or '{}_lsq'.format(self.tag) in self.args.keyword:
+                                            if k in ['level_num']:
+                                                # if hasattr(self, 'clip_val'):
+                                                v = float(v)
+                                                # if negative number provide, it indicates decreasing on current
+                                                v = v if v > 0 else self.level_num.item() + v
 
-                                                    assert v > 1.9, "level_num should be at least 2"
-                                                    scale = (v - 1) / (self.level_num.item() - 1)
-                                                    self.clip_val.mul_(scale)
-                                                    self.verbose('update {}_clip_val to {} for index {}'.format(
-                                                        self.tag, self.clip_val, self.index))
+                                                assert v > 1.9, "level_num should be at least 2"
+                                                scale = (v - 1) / (self.level_num.item() - 1)
+                                                self.clip_val.mul_(scale)
+                                                self.verbose('update {}_clip_val to {} for index {}'.format(
+                                                    self.tag, self.clip_val, self.index))
 
-                                                    # remember to patch the momentum in SGD optimizer. set it to zero or multiple by scale
-                                                    if 'reset_momentum_list' in feedback:
-                                                        feedback['reset_momentum_list'].append(self.clip_val)
-                                                    else:
-                                                        feedback['reset_momentum_list'] = [self.clip_val]
-                                        getattr(self, k).fill_(float(v))
-                                    self.verbose('update {}_{} to {} for index {}'.format(
-                                        self.tag, k, getattr(self, k, 'Non-Exist'), self.index))
-                                else:
-                                    setattr(self, "{}".format(k), v)
-                                    self.verbose('update {}_{} to {} for index {}'.format(
-                                        self.tag, k, getattr(self, k, 'Non-Exist'), self.index))
-                                if self.enable:
-                                    assert hasattr(self, 'iteration'), \
-                                        "cannot enable quantization for current layer. Likely an error in policy file"
-                            # global_buffer
-                            if k in ['global_buffer'] and hasattr(self.args, 'global_buffer'):
-                                v = str(v)
-                                if isinstance(getattr(self.args, k, None), dict) and hasattr(self, v) and self.enable:
-                                    key = "{}-{}-{}".format(v, self.index, self.tag)
-                                    self.args.global_buffer[key] = getattr(self, v)
-                                    self.verbose('update global_buffer (current length: {}), key: {}'.format(
-                                        len(self.args.global_buffer), key))
+                                                # remember to patch the momentum in SGD optimizer. set it to zero or multiple by scale
+                                                if 'reset_momentum_list' in feedback:
+                                                    feedback['reset_momentum_list'].append(self.clip_val)
+                                                else:
+                                                    feedback['reset_momentum_list'] = [self.clip_val]
+                                    getattr(self, k).fill_(float(v))
+                                self.verbose('update {}_{} to {} for index {}'.format(
+                                    self.tag, k, getattr(self, k, 'Non-Exist'), self.index))
+                            else:
+                                setattr(self, "{}".format(k), v)
+                                self.verbose('update {}_{} to {} for index {}'.format(
+                                    self.tag, k, getattr(self, k, 'Non-Exist'), self.index))
+                            if self.enable:
+                                assert hasattr(self, 'iteration'), \
+                                    "cannot enable quantization for current layer. Likely an error in policy file"
+                        # global_buffer
+                        if k in ['global_buffer'] and hasattr(self.args, 'global_buffer'):
+                            v = str(v)
+                            if isinstance(getattr(self.args, k, None), dict) and hasattr(self, v) and self.enable:
+                                key = "{}-{}-{}".format(v, self.index, self.tag)
+                                self.args.global_buffer[key] = getattr(self, v)
+                                self.verbose('update global_buffer (current length: {}), key: {}'.format(
+                                    len(self.args.global_buffer), key))
 
         self.clip_val.requires_grad = self.enable and self.requires_grad
         if not self.enable:
@@ -238,7 +242,7 @@ class Quant(object):
                 setattr(ctx, 'input{}'.format(identifier), y.to(torch.int8))
             elif level == 256 and not signed:
                 setattr(ctx, 'input{}'.format(identifier), y.to(torch.uint8))
-            elif level == 16: # not verified
+            elif level == 16:  # not verified
                 setattr(ctx, 'input{}'.format(identifier), packbit.packbits_padded(y, dim=0, mask=3))
             elif level == 4:  # not verified
                 setattr(ctx, 'input{}'.format(identifier), packbit.packbits_padded(y, dim=0, mask=15))
@@ -255,24 +259,32 @@ class Quant(object):
 
             clip_val = clip_val.abs().to(dtype=x.dtype)
             if non_negative_only:
-                y = x / clip_val * (level-1)
+                y = x / clip_val * (level - 1)
                 y = torch.round(y)
-                y = torch.clamp(y, min=0, max=level-1)
+                y = torch.clamp(y, min=0, max=level - 1)
                 if training:
                     save_for_backward(y, signed=False)
-                y = y / (level-1) * clip_val
-                is_filtered = x >= clip_val
+                y = y / (level - 1) * clip_val
+                # is_filtered = x >= clip_val
+                max_clipped = x >= clip_val
             else:
-                y = x / clip_val * (level//2)
+                y = x / clip_val * (level // 2)
                 y = torch.round(y)
-                y = torch.clamp(y, min=-level//2, max=level-level//2-1)
+                y = torch.clamp(y, min=-level // 2, max=level - level // 2 - 1)
                 if training:
                     save_for_backward(y, signed=True)
-                y = y / (level//2) * clip_val
-                is_filtered = (x >= (clip_val * (level-level//2-1) / (level//2))) | (x <= -clip_val)
+                y = y / (level // 2) * clip_val
+                # is_filtered = (x >= (clip_val * (level-level//2-1) / (level//2))) | (x <= -clip_val)
+                max_clipped = x >= (clip_val * (level - level // 2 - 1) / (level // 2))
+                min_clipped = x <= -clip_val
+
             if training:
-                setattr(ctx, 'is_filtered{}'.format(identifier), packbit.packbits_padded(is_filtered, dim=0))
+                # setattr(ctx, 'is_filtered{}'.format(identifier), packbit.packbits_padded(is_filtered, dim=0))
+                setattr(ctx, 'max_clipped{}'.format(identifier), packbit.packbits_padded(max_clipped, dim=0))
+                if not non_negative_only:
+                    setattr(ctx, 'min_clipped{}'.format(identifier), packbit.packbits_padded(min_clipped, dim=0))
                 setattr(ctx, 'clip_val{}'.format(identifier), clip_val)
+
 
         if training:
             setattr(ctx, 'level{}'.format(identifier), level)
@@ -301,25 +313,47 @@ class Quant(object):
         else:
             clip_val = getattr(ctx, 'clip_val{}'.format(identifier))
             if non_negative_only:
-                y = saved_tensors(input, level, dtype=clip_val.dtype) / (level- 1) * clip_val
+                y = saved_tensors(input, level, dtype=clip_val.dtype) / (level - 1) * clip_val
             else:
-                y = saved_tensors(input, level, dtype=clip_val.dtype) / (level//2) * clip_val
+                y = saved_tensors(input, level, dtype=clip_val.dtype) / (level // 2) * clip_val
         return y
 
     @staticmethod
     def backward(ctx, grad_input, identifier="_"):
+        # pydevd.settrace(suspend=False, trace_only_current_thread=True)
         level = getattr(ctx, 'level{}'.format(identifier))
         if level == 0:
             grad_clip = None
         else:
             clip_val = getattr(ctx, 'clip_val{}'.format(identifier))
-            is_filtered = getattr(ctx, 'is_filtered{}'.format(identifier))
-            is_filtered = packbit.unpackbits_padded(is_filtered, dim=0).to(dtype=torch.bool)
-            grad_clip = grad_input.masked_select(is_filtered).sum().reshape(clip_val.shape)
-            grad_input.masked_fill_(is_filtered, 0.0)
-            setattr(ctx, 'is_filtered{}'.format(identifier), None)
+
+            non_negative_only = getattr(ctx, 'non_negative_only{}'.format(identifier))
+
+            if non_negative_only:
+                max_clipped = getattr(ctx, 'max_clipped{}'.format(identifier))
+                max_clipped = packbit.unpackbits_padded(max_clipped, dim=0).to(dtype=torch.bool)
+                grad_clip = grad_input.masked_select(max_clipped).sum().reshape(clip_val.shape)
+                grad_input.masked_fill_(max_clipped, 0.0)
+                setattr(ctx, 'max_clipped{}'.format(identifier), None)
+            else:
+                max_clipped = getattr(ctx, 'max_clipped{}'.format(identifier))
+                max_clipped = packbit.unpackbits_padded(max_clipped, dim=0).to(dtype=torch.bool)
+
+                min_clipped = getattr(ctx, 'min_clipped{}'.format(identifier))
+                min_clipped = packbit.unpackbits_padded(min_clipped, dim=0).to(dtype=torch.bool)
+
+                grad_clip = grad_input.masked_select(max_clipped).sum() - grad_input.masked_select(min_clipped).sum()
+                grad_clip = grad_clip.reshape(clip_val.shape)
+
+                grad_input.masked_fill_(max_clipped, 0.0)
+                grad_input.masked_fill_(min_clipped, 0.0)
+
+                setattr(ctx, 'max_clipped{}'.format(identifier), None)
+                setattr(ctx, 'min_clipped{}'.format(identifier), None)
+
             setattr(ctx, 'clip_val{}'.format(identifier), None)
         return grad_clip
+
 
 class quantization(nn.Module, Quant):
     def __init__(self, memory_saving=False, args=None, logger=None, tag='fm'):