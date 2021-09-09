caf3a6025fdf12b16ec4d649ea86409eda4ede23
HubHop <zizhengpan98@gmail.com>
2021-09-09 10:49:35 +1000

    fix cuda for channel wise quantization

--- memory_saving/cpp_extension/quantization_cuda_kernel.cu
+++ memory_saving/cpp_extension/quantization_cuda_kernel.cu
@@ -11,7 +11,7 @@
 #include <curand_kernel.h>
 
 #define BLOCK_Y_DIM_MAX ((((int64_t)(1)) << 16) - 1)
-// #define fmax(a, b) ((a) > (b) ? (a): (b))
+#define fmax(a, b) ((a) > (b) ? (a): (b))
 // #define fmin(a, b) ((a) < (b) ? (a): (b))
 
 using torch::IntArrayRef;
@@ -24,30 +24,22 @@ __global__ void pack_single_precision_kernel(int32_t bits,
                                              const scalar_t* __restrict__ data,
                                              const scalar_t* __restrict__ scale,
                                              const scalar_t* __restrict__ shift,
-                                             int8_t* __restrict__ packed,
-                                             std::pair<uint64_t, uint64_t> seeds,
-                                             int64_t N,
-                                             int64_t num_groups,
-                                             int64_t group_size,
-                                             int64_t block_idx_y_base) {
-  const int64_t no = blockIdx.y + block_idx_y_base;
-  const int group_id = blockIdx.x;
-  const int d = threadIdx.x;
-  const int64_t global_thread_id = (no * num_groups + group_id) * group_size + d;
+                                             int8_t* __restrict__ packed
+//                                              std::pair<uint64_t, uint64_t> seeds
+                                            ) {
+  const int64_t global_thread_id = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
 
-  curandStatePhilox4_32_10_t state;
-  curand_init(seeds.first, global_thread_id, seeds.second, &state);
+//   curandStatePhilox4_32_10_t state;
+//   curand_init(seeds.first, global_thread_id, seeds.second, &state);
+//   const float noise = curand_uniform(&state);
 
-  const int64_t id = (no * num_groups + group_id) * group_size + d;
-  const float noise = curand_uniform(&state);
-
-// //  mine
-//   auto quant_int_val = lrintf((data[id] - shift[group_id]) * scale[group_id] + noise - 0.5);
-//   uint8_t local_packed = fmax(0, fmin(255, quant_int_val));
+  const int64_t id = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
 
 //   previous
   uint8_t local_packed = 0;
-  const int32_t val = __float2int_rn(fmaxf((data[id] - shift[group_id]) * scale[group_id] + noise - 0.5, 0.0f));
+  const int32_t val = __float2int_rn(fmaxf((data[id] - shift[threadIdx.x]), 0.0f));
+//   const int32_t val = __float2int_rn(fmax((data[id] - shift[threadIdx.x]) * scale[threadIdx.x] + noise - 0.5, 0.0f));
+//   const int32_t val = __float2int_rn(fmaxf((data[id] - shift[threadIdx.x]) * scale[threadIdx.x], 0.0f));
   local_packed |= val;
   packed[global_thread_id] = local_packed;
 }
@@ -57,46 +49,56 @@ Tensor pack_single_precision_cuda(Tensor data,
                                   Tensor scale,
                                   Tensor shift,
                                   int bits,
-                                  bool stochastic, 
-                                  int batch_size) {
-  int64_t N = data.size(0);
-  int64_t num_groups = data.size(1);
-  int64_t group_size = batch_size;
+                                  bool stochastic) {
+  int64_t B = data.size(0);
+  int64_t N = data.size(1);
+  int64_t C = data.size(2);
 
   // Compute total bits
   TORCH_CHECK(8 % bits == 0);
 
-  int64_t total_bits = (int64_t)bits * (N * num_groups);
+  int64_t total_bits = (int64_t)bits * (B * N * C);
   auto options = torch::TensorOptions().dtype(torch::kInt8).device(data.device());
   Tensor packed = torch::empty({(total_bits + 8) / 8,}, options);
 
-  // Random number generator
-  auto gen = at::check_generator<at::CUDAGeneratorImpl>(at::cuda::detail::getDefaultCUDAGenerator());
-  std::pair<uint64_t, uint64_t> rng_engine_inputs;
-  {
-    // See Note [Acquire lock when using random generators]
-    std::lock_guard<std::mutex> lock(gen->mutex_);
-    rng_engine_inputs = gen->philox_engine_inputs(256);
-  }
-  TORCH_CHECK(stochastic);
+//   // Random number generator
+//   auto gen = at::check_generator<at::CUDAGeneratorImpl>(at::cuda::detail::getDefaultCUDAGenerator());
+//   std::pair<uint64_t, uint64_t> rng_engine_inputs;
+//   {
+//     // See Note [Acquire lock when using random generators]
+//     std::lock_guard<std::mutex> lock(gen->mutex_);
+//     rng_engine_inputs = gen->philox_engine_inputs(C);
+//   }
+//   TORCH_CHECK(stochastic);
 
+  dim3 block_dim(N, B, 1);
+  dim3 thread_dim(C, 1, 1);
 
-  int64_t needed_blocks_y = N/group_size;
-  // Call pack kernels
-  for (int64_t block_idx_y_base = 0; block_idx_y_base < needed_blocks_y; block_idx_y_base += BLOCK_Y_DIM_MAX) {
-    dim3 block_dim(num_groups, std::min(needed_blocks_y - block_idx_y_base, BLOCK_Y_DIM_MAX), 1);
-    dim3 thread_dim(group_size, 1, 1);
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(data.scalar_type(), "pack_single_precision", ([&] {
+    pack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
+      bits,
+      data.data_ptr<scalar_t>(),
+      scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
+      packed.data_ptr<int8_t>()
+//       rng_engine_inputs
+      );
+  }));
+  // int64_t needed_blocks_y = N/group_size;
+  // // Call pack kernels
+  // for (int64_t block_idx_y_base = 0; block_idx_y_base < needed_blocks_y; block_idx_y_base += BLOCK_Y_DIM_MAX) {
+  //   dim3 block_dim(N, std::min(needed_blocks_y - block_idx_y_base, BLOCK_Y_DIM_MAX), 1);
+  //   dim3 thread_dim(group_size, 1, 1);
 
-    AT_DISPATCH_FLOATING_TYPES_AND_HALF(data.scalar_type(), "pack_single_precision", ([&] {
-      pack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
-        bits,
-        data.data_ptr<scalar_t>(),
-        scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
-        packed.data_ptr<int8_t>(),
-        rng_engine_inputs,
-        N, num_groups, group_size, block_idx_y_base);
-    }));
-  }
+  //   AT_DISPATCH_FLOATING_TYPES_AND_HALF(data.scalar_type(), "pack_single_precision", ([&] {
+  //     pack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
+  //       bits,
+  //       data.data_ptr<scalar_t>(),
+  //       scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
+  //       packed.data_ptr<int8_t>(),
+  //       // rng_engine_inputs,
+  //       N, num_groups, group_size, block_idx_y_base);
+  //   }));
+  // }
 
   return packed;
 }
@@ -107,21 +109,19 @@ __global__ void unpack_single_precision_kernel(int32_t bits,
                                                const int8_t* __restrict__ data,
                                                const scalar_t* __restrict__ scale,
                                                const scalar_t* __restrict__ shift,
-                                               scalar_t* __restrict__ unpacked,
-                                               int64_t N,
-                                               int64_t num_groups,
-                                               int64_t group_size,
-                                               int64_t block_idx_y_base) {
-  const int64_t no = blockIdx.y + block_idx_y_base;
-  const int group_id = blockIdx.x;
-  const int d = threadIdx.x;
-  const int64_t global_thread_id = (no * num_groups + group_id) * group_size + d;
+                                               scalar_t* __restrict__ unpacked) {
+  // const int64_t no = blockIdx.y + block_idx_y_base;
+  // const int group_id = blockIdx.x;
+  // const int d = threadIdx.x;
+  // const int64_t global_thread_id = (no * num_groups + group_id) * group_size + d;
+  const int64_t global_thread_id = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
 
   uint8_t local_packed = data[global_thread_id];
   int mask = ((1 << bits) - 1);
   const int val = local_packed & mask;
-  const int64_t id = (no * num_groups + group_id) * group_size + d;
-  unpacked[id] = ((scalar_t)val) / scale[group_id] + shift[group_id];
+  const int64_t id = (blockIdx.y * gridDim.x + blockIdx.x) * blockDim.x + threadIdx.x;
+//   unpacked[id] = ((scalar_t)val) / scale[threadIdx.x] + shift[threadIdx.x];
+  unpacked[id] = ((scalar_t)val) + shift[threadIdx.x];
 }
 
 // Unpack int32 bit stream to float16/32 data
@@ -129,26 +129,40 @@ Tensor unpack_single_precision_cuda(Tensor data,
                                     int bits,
                                     Tensor scale,
                                     Tensor shift,
+                                    int64_t B,
                                     int64_t N,
-                                    int64_t num_groups,
-                                    int64_t group_size) {
+                                    int64_t C) {
   auto options = torch::TensorOptions().dtype(scale.dtype()).device(data.device());
-  Tensor unpacked = torch::empty({N, num_groups}, options);
+  Tensor unpacked = torch::empty({B, N, C}, options);
 
-  int64_t needed_blocks_y = N/group_size;
-  for (int64_t block_idx_y_base = 0; block_idx_y_base < needed_blocks_y; block_idx_y_base += BLOCK_Y_DIM_MAX) {
-    dim3 block_dim(num_groups, std::min(needed_blocks_y - block_idx_y_base, BLOCK_Y_DIM_MAX), 1);
-    dim3 thread_dim(group_size, 1, 1);
+  // dim3 block_dim(num_groups, std::min(needed_blocks_y - block_idx_y_base, BLOCK_Y_DIM_MAX), 1);
+  // dim3 thread_dim(group_size, 1, 1);
+  dim3 block_dim(N, B, 1);
+  dim3 thread_dim(C, 1, 1);
 
-    AT_DISPATCH_FLOATING_TYPES_AND_HALF(scale.scalar_type(), "unpack_single_precision", ([&] {
-      unpack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
-        bits,
-        data.data_ptr<int8_t>(),
-        scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
-        unpacked.data_ptr<scalar_t>(),
-        N, num_groups, group_size, block_idx_y_base);
-    }));
-  }
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(scale.scalar_type(), "unpack_single_precision", ([&] {
+    unpack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
+      bits,
+      data.data_ptr<int8_t>(),
+      scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
+      unpacked.data_ptr<scalar_t>()
+      );
+  }));
+
+  // int64_t needed_blocks_y = N/group_size;
+  // for (int64_t block_idx_y_base = 0; block_idx_y_base < needed_blocks_y; block_idx_y_base += BLOCK_Y_DIM_MAX) {
+  //   dim3 block_dim(num_groups, std::min(needed_blocks_y - block_idx_y_base, BLOCK_Y_DIM_MAX), 1);
+  //   dim3 thread_dim(group_size, 1, 1);
+
+  //   AT_DISPATCH_FLOATING_TYPES_AND_HALF(scale.scalar_type(), "unpack_single_precision", ([&] {
+  //     unpack_single_precision_kernel<scalar_t, false><<<block_dim, thread_dim>>>(
+  //       bits,
+  //       data.data_ptr<int8_t>(),
+  //       scale.data_ptr<scalar_t>(), shift.data_ptr<scalar_t>(),
+  //       unpacked.data_ptr<scalar_t>(),
+  //       N, num_groups, group_size, block_idx_y_base);
+  //   }));
+  // }
 
   return unpacked;
 }
--- memory_saving/test_quant.py
+++ memory_saving/test_quant.py
@@ -3,26 +3,92 @@ import time
 import torch
 import numpy as np
 
+def check_quant_element_v2():
+    data = torch.zeros(2, 2, 4).cuda()
+    B, N, C = data.shape
+    data[:, :, 0] = 2.
+    data[:, :, 1] = 4.
+    data[:, :, 2] = 8.
+    data[:, :, 3] = 16.
+    # data = torch.zeros(3, 4, 2).cuda()
+    # data[0, :, :] = 2.
+    # data[1, :, :] = 4.
+    # data[2, :, :] = 8.
+    print(data)
+    # temp = data.permute(0, 2, 1).reshape(-1, 3)
+
+    mins, _ = data.min(0)
+    maxs, _ = data.max(0)
+    # maxs = torch.amax(data, (1))
+    # mins = torch.amin(data, (1))
+
+    test_shift = torch.tensor([2., 4., 8., 16.]).cuda()
+
+    level = 255
+    scale = maxs - mins + 2e-6
+    cuda_scales = 255 / scale
+
+    # N, num_groups = data.shape
+
+    output = ext_quant.pack_single_precision(data, cuda_scales, test_shift, 8, True)
+    print(output)
+    dequant_out = ext_quant.unpack_single_precision(output, 8, cuda_scales, test_shift, B, N, C)
+    print(dequant_out)
+    print(torch.all(torch.eq(data, dequant_out)).item())
+    # assert data == dequant_out
+
+def check_quant_element():
+    data = torch.zeros(2, 4, 64).cuda()
+    B, N, C = data.shape
+    data[:, :, 0] = 2.
+    data[:, :, 1] = 4.
+    data[:, :, 2] = 8.
+    # data = torch.zeros(3, 4, 2).cuda()
+    # data[0, :, :] = 2.
+    # data[1, :, :] = 4.
+    # data[2, :, :] = 8.
+    print(data)
+    # temp = data.permute(0, 2, 1).reshape(-1, 3)
+
+    mins, _ = data.min(0)
+    maxs, _ = data.max(0)
+    print(mins)
+    # maxs = torch.amax(data, (1))
+    # mins = torch.amin(data, (1))
+
+    test_shift = torch.tensor([2., 4., 8.]).cuda()
+
+    level = 255
+    scale = maxs - mins + 2e-6
+    cuda_scales = 255 / scale
+
+    # N, num_groups = data.shape
+
+    output = ext_quant.pack_single_precision(data, cuda_scales, test_shift, 8, True, 2)
+    print(output)
+    dequant_out = ext_quant.unpack_single_precision(output, 8, cuda_scales, test_shift, B, N, C)
+    print(dequant_out)
+    print('..')
+
+
 def check_quant():
     diff = 0.
-
-
     for i in range(200):
-        data = torch.randn(128*197*197, 3).cuda()
+        data = torch.randn(128, 197, 1024).cuda()
         # temp = data.permute(0, 2, 1).reshape(-1, 3)
 
-        mins, _ = data.min(0)
-        maxs, _ = data.max(0)
-
+        # mins, _ = data.min(0)
+        # maxs, _ = data.max(0)
+        mins = torch.amin(data, (0, 1))
+        maxs = torch.amax(data, (0, 1))
         level = 255
         scale = maxs - mins + 2e-6
         cuda_scales = 255 / scale
 
-        N, num_groups = data.shape
+        B, N, C = data.shape
 
-        output = ext_quant.pack_single_precision(data, cuda_scales, mins, 8, True, 128)
-        dequant_out = ext_quant.unpack_single_precision(output, 8, cuda_scales, mins, N, num_groups,
-                                                        128)
+        output = ext_quant.pack_single_precision(data, cuda_scales, mins, 8, True)
+        dequant_out = ext_quant.unpack_single_precision(output, 8, cuda_scales, mins, B, N, C)
         cuda_quant_error = (dequant_out - data).norm()
 
         y = (data - mins) / scale * (level - 1)
@@ -34,7 +100,7 @@ def check_quant():
         cur_error = (cuda_quant_error - torch_quant_error).item()
         print(cur_error)
         diff += cur_error
-    print('diff = ', diff/200)
+    print('diff = ', diff / 200)
 
 
 def check_correctness():
@@ -73,8 +139,9 @@ def check_correctness():
         # clip_val = maxs - mins
         y = (temp - mins) / scale * (level - 1)
         # y = torch.clamp(y, min=0)
-        y = torch.clamp(y, min=0, max=level - 1)
         y = torch.round(y)
+        y = torch.clamp(y, min=0, max=level - 1)
+
         # y = torch.clamp(y, min=0, max=level - 1)
 
         torch_dequant_out = y / (level - 1) * scale + mins
@@ -83,7 +150,8 @@ def check_correctness():
         diff += (cuda_quant_error - torch_quant_error).item()
 
         # print('torch quant error = ', torch_quant_error)
-    print('diff = ', diff/100)
+    print('diff = ', diff / 100)
+
 
 def cuda_quant(data, mins, scales):
     # cuda_scales = 255 / (maxs - mins + 2e-6)
@@ -94,10 +162,12 @@ def cuda_quant(data, mins, scales):
     output = ext_quant.pack_single_precision(data, scales, mins, 8, True)
     # dequant_out = ext_quant.unpack_single_precision(output, 8, scales, mins, N, num_features // group_size, group_size)
 
+
 def cuda_quant2(data, mins, scales):
     # cuda_scales = 255 / (maxs - mins + 2e-6)
-    output = ext_quant.pack_single_precision(data, scales, mins, 8, True, 128)
+    output = ext_quant.pack_single_precision(data, scales, mins, 8, True)
     # print('..')
+
 
 def torch_quant(data, mins, scales):
     # torch
@@ -108,9 +178,10 @@ def torch_quant(data, mins, scales):
 
     # torch_dequant_out = y / scales + mins
 
+
 def test_cuda():
     # Torch Forward: 9015.575 us
-    cuda_data = torch.randn(197*197, 3, 128).cuda()
+    cuda_data = torch.randn(197 * 197, 3, 128).cuda()
     torch_data = cuda_data.permute(0, 2, 1).reshape(-1, 3)
 
     mins, _ = torch_data.min(0)
@@ -120,7 +191,8 @@ def test_cuda():
 
 
 if __name__ == '__main__':
-    check_quant()
+    check_quant_element_v2()
+    # check_quant()
     # test_cuda()
     # # CUDA Forward: 61.892 us
     # # Torch Forward: 200.598 us
@@ -152,23 +224,66 @@ if __name__ == '__main__':
     # __float2int_rn
     # CUDA Forward: 92.401 us
     # Torch Forward: 584.516 us
-    data = torch.randn(197 * 197 * 128, 3).cuda()
 
-    mins, _ = data.min(0)
-    maxs, _ = data.max(0)
+    # channel wise quantization
+    # data = torch.randn(128, 197, 192).cuda()
+    # 192
+    # CUDA Forward: 31.143 us
+    # Torch Forward: 195.919 us
 
-    scales = 255 / (maxs - mins + 2e-6)
+    # 192 with noise
+    # CUDA Forward: 59.587 us
+    # Torch Forward: 199.620 us
 
-    forward = 0
-    for _ in range(100000):
-        start = time.time()
-        cuda_quant2(data, mins, scales)
-        forward += time.time() - start
-    print('CUDA Forward: {:.3f} us'.format(forward * 1e6 / 1e5))
+    # 384
+    # CUDA Forward: 60.601 us
+    # Torch Forward: 380.579 us
 
-    forward = 0
-    for _ in range(10000):
-        start = time.time()
-        torch_quant(data, mins, scales)
-        forward += time.time() - start
-    print('Torch Forward: {:.3f} us'.format(forward * 1e6 / 1e4))
+    # 384 with noise
+    # CUDA Forward: 118.746 us
+    # Torch Forward: 388.483 us
+
+    # 768
+    # CUDA Forward: 123.359 us
+    # Torch Forward: 753.552 us
+
+    # 768 with noise
+    # CUDA Forward: 252.021 us
+    # Torch Forward: 768.403 us
+
+    # without noise
+    # data = torch.randn(128, 3136, 96).cuda()
+    # CUDA Forward: 260.118 us
+    # Torch Forward: 1494.353 us
+
+    # data = torch.randn(128, 784, 192).cuda()
+    # CUDA Forward: 117.771 us
+    # Torch Forward: 752.368 us
+
+    # data = torch.randn(128, 196, 384).cuda()
+    # CUDA Forward: 60.966 us
+    # Torch Forward: 380.124 us
+
+    # data = torch.randn(128, 49, 768).cuda()
+    # CUDA Forward: 32.774 us
+    # Torch Forward: 196.369 us
+    # data = torch.randn(128, 49, 768).cuda()
+    #
+    # mins = torch.amin(data, (0, 1))
+    # maxs = torch.amax(data, (0, 1))
+    #
+    # scales = 255 / (maxs - mins + 2e-6)
+    #
+    # forward = 0
+    # for _ in range(100000):
+    #     start = time.time()
+    #     cuda_quant2(data, mins, scales)
+    #     forward += time.time() - start
+    # print('CUDA Forward: {:.3f} us'.format(forward * 1e6 / 1e5))
+    #
+    # forward = 0
+    # for _ in range(10000):
+    #     start = time.time()
+    #     torch_quant(data, mins, scales)
+    #     forward += time.time() - start
+    # print('Torch Forward: {:.3f} us'.format(forward * 1e6 / 1e4))
