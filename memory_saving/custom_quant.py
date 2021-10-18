cb4490be261741dc68ed6f2666f92debe4381f97
HubHop <zizhengpan98@gmail.com>
2021-10-18 19:19:29 +1100

    rollback clip_range

--- memory_saving/custom_quant.py
+++ memory_saving/custom_quant.py
@@ -76,7 +76,8 @@ def depack_group(x, groups, input_shape):
 def update_clip_val_shift(input, clip_val, shift, iteration, ema_decay, level):
     max_value = torch.amax(input, 1)
     min_value = torch.amin(input, 1)
-    clip_range = torch.clamp(max_value - min_value, None, 1024)
+    # clip_range = torch.clamp(max_value - min_value, None, 1024)
+    clip_range = max_value - min_value
     if iteration == 0:
         clip_val.data = clip_range
         shift.data = min_value
